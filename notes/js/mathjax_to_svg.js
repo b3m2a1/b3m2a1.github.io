78ba989fbc94ae1b49b96f0cfb27977225925544
b3m2a1 <b3m2a1@gmail.com>
2025-05-06 19:23:17 -0500

    mathjax to SVG exporter updates

--- notes/js/mathjax_to_svg.js
+++ notes/js/mathjax_to_svg.js
@@ -1,7 +1,9 @@
 //Adapted from https://www.mathjax.org/js/main.js
 
 window.MathJax = {
-  loader: {load: ['input/asciimath']},
+  loader: {
+    load: ['input/asciimath', '[tex]/color'],
+  },
   startup: {
     pageReady: function () {
       //
@@ -41,6 +43,55 @@ window.MathJax = {
             button.disabled = false;
           });
         }
+
+        window.saveResult = function() {
+
+          let renderer = document.getElementById('Renderer').value
+          let node = document.getElementById('MathPreview')
+          
+          if (renderer === 'SVG') {
+            node = node.getElementsByTagName("svg")[0]
+            node.style['color'] = 'black'
+          }
+
+          //get svg source.
+          let serializer = new XMLSerializer();
+          let source = serializer.serializeToString(node);
+
+          
+          let fmt = "data:text/html;charset=utf-8";
+          let extension = ".html"
+          if (renderer === 'SVG') {
+            //add name spaces.
+            if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+              source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+            }
+            if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+              source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+            }
+            
+            //add xml declaration
+            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+            
+            //convert svg source to URI data scheme.
+            fmt = "data:image/svg+xml;charset=utf-8";
+            extension = ".svg"
+          }
+          let svgBlob = new Blob([source], {type:fmt});
+          let svgUrl = URL.createObjectURL(svgBlob);
+          let downloadLink = document.createElement("a");
+          downloadLink.href = svgUrl;
+
+          let fileName = document.getElementById('fileName').value
+          if (!fileName.length) {
+            fileName = "tex_output"
+          }
+          downloadLink.download = fileName + extension;
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+        }
+
         input.oninput = typesetInput;
       }
 
